2ec24f4f5903cc322f873ce37c034417fea1ed4e
Siddharth Umachandar <siddharth.u@somaiya.edu>
2024-11-03 03:30:24 -0500

    latest code

--- chatbot/views.py
+++ chatbot/views.py
@@ -8,12 +8,8 @@ class ResponseSchema(Schema):
 router = Router()
 chatbot = ChatBot()
 
-@router.post('/message')
+@router.post('/message', response=ResponseSchema)
 def get_message(request, characters: str = '', message: str = ''):
     chatbot.set_pramater(characters, message)
-    return {"message": "Success"}
-
-@router.post('/generate_message', response=ResponseSchema)
-def generate_message(request):
     response = chatbot.response()
     return response
