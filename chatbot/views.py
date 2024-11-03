4220e5f3f8c6b470983d43be76f13bd1ad16f32f
Siddharth Umachandar <siddharth.u@somaiya.edu>
2024-11-03 07:31:14 -0500

    final update

--- chatbot/views.py
+++ chatbot/views.py
@@ -5,11 +5,16 @@ from .Chatbot_Logic import ChatBot
 class ResponseSchema(Schema):
     message: str
 
+class MessageSchema(Schema):
+    characters: str
+    message: str
+
 router = Router()
 chatbot = ChatBot()
 
 @router.post('/message', response=ResponseSchema)
-def get_message(request, characters: str = '', message: str = ''):
-    chatbot.set_pramater(characters, message)
+def get_message(request, data: MessageSchema):
+    chatbot.set_pramater(data.characters, data.message)
     response = chatbot.response()
+    print(response)
     return response
